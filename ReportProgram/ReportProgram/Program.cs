cee397402c2f16a54625eae1fdb9023846e446ab
William B McAllister <blakemcallister77@gmail.com>
2018-09-18 21:40:35 -0400

    reportProgram

---
+++ ReportProgram/ReportProgram/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Tech Academy");
+            Console.WriteLine("Student Daily Report");
+            Console.WriteLine("What course are you on?");
+            string course = Console.ReadLine();
+            Console.WriteLine("What page number?");
+            string pageNum = Console.ReadLine();
+            Console.WriteLine("Do you need help with anything? Please answer true or false");
+            string help = Console.ReadLine();
+            Console.WriteLine("Where there any positive experiences you'd like to share? Please give specifics.");
+            string comment = Console.ReadLine();
+            Console.WriteLine("Is there any other feedback you'd like to provide?");
+            string feedback = Console.ReadLine();
+            Console.WriteLine("How many hours did you study today?");
+            string studyHours = Console.ReadLine();
+            Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
+            Console.ReadLine();
+
+        }
+    }
+}
